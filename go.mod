a28445ab9d4c7c4d91000c0e1fea852db34bd87e
Thanh Tran <tranvanthanh@gapo.com.vn>
2021-02-26 18:46:03 +0700

    initial project

---
+++ go.mod
@@ -0,0 +1,14 @@
+module go-bank-transfer
+
+go 1.14
+
+require (
+	github.com/go-playground/locales v0.13.0 // indirect
+	github.com/go-playground/validator/v10 v10.4.1 // indirect
+	github.com/lib/pq v1.9.0 // indirect
+	github.com/satori/go.uuid v1.2.0 // indirect
+	github.com/sirupsen/logrus v1.8.0 // indirect
+	go.mongodb.org/mongo-driver v1.4.6 // indirect
+	go.uber.org/zap v1.16.0 // indirect
+	gopkg.in/mgo.v2 v2.0.0-20190816093944-a6b53ec6cb22 // indirect
+)
---
+++ infrastructure/log/factory.go
@@ -0,0 +1,28 @@
+package log
+
+import (
+	"errors"
+
+	"go-bank-transfer/adapter/logger"
+)
+
+const (
+	InstanceZapLogger int = iota
+	InstanceLogrusLogger
+)
+
+var (
+	errInvalidLoggerInstance = errors.New("invalid log instance")
+)
+
+func NewLoggerFactory(instance int) (logger.Logger, error) {
+	switch instance {
+	case InstanceZapLogger:
+		return NewZapLogger()
+
+	case InstanceLogrusLogger:
+		return NewLogrusLogger(), nil
+	default:
+		return nil, errInvalidLoggerInstance
+	}
+}
